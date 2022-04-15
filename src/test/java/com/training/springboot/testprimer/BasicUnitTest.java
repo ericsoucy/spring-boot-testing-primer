cbcf58b47feb8773feb5a9905e2fbb90ecbeece3
eric soucy <eric.soucy@gmail.com>
2022-04-15 17:54:32 -0400

    maven tests: Unit + integration

---
+++ src/test/java/com/training/springboot/testprimer/BasicUnitTest.java
@@ -0,0 +1,11 @@
+package com.training.springboot.testprimer;
+
+import org.junit.jupiter.api.Test;
+
+class BasicUnitTest {
+
+  @Test
+  void test() {
+    System.out.println("... running a unit test with the Maven Surefire Plugin");
+  }
+}
---
+++ src/test/java/com/training/springboot/testprimer/TestBasicIT.java
@@ -0,0 +1,10 @@
+package com.training.springboot.testprimer;
+
+import org.junit.jupiter.api.Test;
+
+class TestBasicIT {
+  @Test
+  void test() {
+    System.out.println("... running a unit test with the failsafe Plugin");
+  }
+}
